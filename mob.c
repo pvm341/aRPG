220edee9628c07ff302705548bb966bc15b3583c
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-09-02 23:43:46 +0100

    mob.c and mob.h added

---
+++ mob.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/* Helper functions */
+
+/* CRUD functionality */
+void create_mob(void){
+}
+
+void retrieve_mob(void){
+}
+
+void update_mob(void){
+}
+
+void delete_mob(void){
+}
+
---
+++ mob.h
@@ -0,0 +1,15 @@
+
+#include "list.h"
+#include "inventory.h"
+
+struct smob {
+  char name[26];
+  int no_items_in_inventory;
+  pll inventory;
+};
+
+typedef struct smob tmob, *pmob, **dmob;
+void create_mob(void);
+void retrieve_mob(void);
+void update_mob(void);
+void delete_mob(void);
